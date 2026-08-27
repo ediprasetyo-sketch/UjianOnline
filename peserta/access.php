341cf67b54261976972079cc9f4f1c3b7d502290
ediprasetyo-sketch <edi.prasetyo@revoprintshop.com>
2026-08-27 17:12:49 +0700

    fix: remove duplicate session_start from participant access

--- peserta/access.php
+++ peserta/access.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-if(session_status()!==PHP_SESSION_ACTIVE) session_start();
 require __DIR__.'/../config.php';
 require_once __DIR__.'/../includes/participant_session.php';
 
